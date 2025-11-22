b74eec9338ab2f72ef3d65899eef2caa1813def5
chinhdang5504 <chinh.dang5504@gmail.com>
2025-11-22 21:10:44 +0700

    initial user when start project

--- src/main/java/com/example/sso_demo/config/ApplicationInitConfig.java
+++ src/main/java/com/example/sso_demo/config/ApplicationInitConfig.java
@@ -17,8 +17,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
 
-import static com.example.sso_demo.config.PredefinedRole.ADMIN_ROLE;
-
 @Configuration
 @RequiredArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
@@ -36,31 +34,94 @@ public class ApplicationInitConfig {
         log.info("Initializing application.....");
         return args -> {
             if (userRepository.findByUsername(ADMIN_USER_NAME).isEmpty()) {
-                roleRepository.save(Role.builder()
-                        .name(PredefinedRole.USER_ROLE)
-                        .description("User role")
-                        .build());
+                // Create roles
+                Role userRole = roleRepository
+                        .save(Role.builder().name(PredefinedRole.USER_ROLE).description("User role").build());
+                Role adminRole = roleRepository
+                        .save(Role.builder().name(PredefinedRole.ADMIN_ROLE).description("Admin role").build());
+                Role studentRole = roleRepository
+                        .save(Role.builder().name(PredefinedRole.STUDENT_ROLE).description("Student role").build());
+                Role tutorRole = roleRepository
+                        .save(Role.builder().name(PredefinedRole.TUTOR_ROLE).description("Tutor role").build());
+                Role headerRole = roleRepository
+                        .save(Role.builder().name(PredefinedRole.HEADER_ROLE).description("Header role").build());
+                Role managerRole = roleRepository
+                        .save(Role.builder().name(PredefinedRole.MANAGER_ROLE).description("Manager role").build());
+                Role coordinatorRole = roleRepository.save(
+                        Role.builder().name(PredefinedRole.COORDINATOR_ROLE).description("Coordinator role").build());
 
-                Role adminRole = roleRepository.save(Role.builder()
-                        .name(ADMIN_ROLE)
-                        .description("Admin role")
-                        .build());
-
-                var roles = new HashSet<Role>();
-                roles.add(adminRole);
-
-                User user = User.builder()
+                // Create Admin
+                var adminRoles = new HashSet<Role>();
+                adminRoles.add(adminRole);
+                User admin = User.builder()
                         .username(ADMIN_USER_NAME)
                         .password(passwordEncoder.encode(ADMIN_PASSWORD))
                         .ms("CB-IT-001")
-                        .roles(roles)
+                        .roles(adminRoles)
                         .build();
-
-                userRepository.save(user);
+                userRepository.save(admin);
                 log.warn("admin user has been created with default password: admin, please change it");
+
+                // Create 5 Students
+                for (int i = 1; i <= 5; i++) {
+                    var studentRoles = new HashSet<Role>();
+                    studentRoles.add(studentRole);
+                    User student = User.builder()
+                            .username("student" + i)
+                            .password(passwordEncoder.encode("password"))
+                            .ms("STU-00" + i)
+                            .roles(studentRoles)
+                            .build();
+                    userRepository.save(student);
+                }
+
+                // Create 5 Tutors
+                for (int i = 1; i <= 5; i++) {
+                    var tutorRoles = new HashSet<Role>();
+                    tutorRoles.add(tutorRole);
+                    User tutor = User.builder()
+                            .username("tutor" + i)
+                            .password(passwordEncoder.encode("password"))
+                            .ms("TUT-00" + i)
+                            .roles(tutorRoles)
+                            .build();
+                    userRepository.save(tutor);
+                }
+
+                // Create 1 Header
+                var headerRoles = new HashSet<Role>();
+                headerRoles.add(headerRole);
+                User header = User.builder()
+                        .username("header")
+                        .password(passwordEncoder.encode("password"))
+                        .ms("HEA-001")
+                        .roles(headerRoles)
+                        .build();
+                userRepository.save(header);
+
+                // Create 1 Manager
+                var managerRoles = new HashSet<Role>();
+                managerRoles.add(managerRole);
+                User manager = User.builder()
+                        .username("manager")
+                        .password(passwordEncoder.encode("password"))
+                        .ms("MAN-001")
+                        .roles(managerRoles)
+                        .build();
+                userRepository.save(manager);
+
+                // Create 1 Coordinator
+                var coordinatorRoles = new HashSet<Role>();
+                coordinatorRoles.add(coordinatorRole);
+                User coordinator = User.builder()
+                        .username("coordinator")
+                        .password(passwordEncoder.encode("password"))
+                        .ms("COO-001")
+                        .roles(coordinatorRoles)
+                        .build();
+                userRepository.save(coordinator);
             }
             log.info("Application initialization completed .....");
         };
     }
 }
-
--- src/main/java/com/example/sso_demo/config/PredefinedRole.java
+++ src/main/java/com/example/sso_demo/config/PredefinedRole.java
@@ -3,6 +3,12 @@ package com.example.sso_demo.config;
 public class PredefinedRole {
     public static final String USER_ROLE = "USER";
     public static final String ADMIN_ROLE = "ADMIN";
+    public static final String STUDENT_ROLE = "STUDENT";
+    public static final String TUTOR_ROLE = "TUTOR";
+    public static final String HEADER_ROLE = "HEADER";
+    public static final String MANAGER_ROLE = "MANAGER";
+    public static final String COORDINATOR_ROLE = "COORDINATOR";
 
-    private PredefinedRole() {}
+    private PredefinedRole() {
+    }
 }
